e09b7af9190209a62e23a0c2dee157ba8eda2fa5
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 19:39:25 -0600

    Update Last Updated date

--- 26-Get-M365DefenderThreatProtectionReport.ps1
+++ 26-Get-M365DefenderThreatProtectionReport.ps1
@@ -3,7 +3,7 @@
 Script Name: 26-Get-M365DefenderThreatProtectionReport.ps1
 Description: Microsoft Defender for Office 365 threat protection analytics
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 REQUIREMENTS:
