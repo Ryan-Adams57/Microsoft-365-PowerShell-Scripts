b0a2ecb8d304a9d3e978082112a6ef4acf60555c
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 19:20:13 -0600

    Update Last Updated date

--- 04-Get-M365MailboxSizeReport.ps1
+++ 04-Get-M365MailboxSizeReport.ps1
@@ -3,7 +3,7 @@
 Script Name: 4-Get-M365MailboxSizeReport.ps1
 Description: Comprehensive mailbox size, quota, and item count report
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 SCRIPT HIGHLIGHTS:
