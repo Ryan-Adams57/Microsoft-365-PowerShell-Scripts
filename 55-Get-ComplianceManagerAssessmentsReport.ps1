4f86386b351668fadc8e7489aa79c9c4e146800f
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 20:03:06 -0600

    Update Last Updated date

--- 55-Get-ComplianceManagerAssessmentsReport.ps1
+++ 55-Get-ComplianceManagerAssessmentsReport.ps1
@@ -3,7 +3,7 @@
 Script Name: 55-Get-ComplianceManagerAssessmentsReport.ps1
 Description: Microsoft Compliance Manager assessments and compliance score report
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 REQUIREMENTS:
