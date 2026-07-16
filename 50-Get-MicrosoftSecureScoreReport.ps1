e23c3a61ddf429f77985be7fcf7c6afdbf79029c
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 19:58:57 -0600

    Update Last Updated date

--- 50-Get-MicrosoftSecureScoreReport.ps1
+++ 50-Get-MicrosoftSecureScoreReport.ps1
@@ -3,7 +3,7 @@
 Script Name: 50-Get-MicrosoftSecureScoreReport.ps1
 Description: Microsoft 365 Advanced Reporting Script 50
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 REQUIREMENTS:
