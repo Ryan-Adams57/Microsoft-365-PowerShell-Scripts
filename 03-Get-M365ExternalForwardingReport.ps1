65ff72bcf10d998c594a7a5740a83c362a8f2536
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 19:19:11 -0600

    Update Last Updated date

--- 03-Get-M365ExternalForwardingReport.ps1
+++ 03-Get-M365ExternalForwardingReport.ps1
@@ -3,7 +3,7 @@
 Script Name: 3-Get-M365ExternalForwardingReport.ps1
 Description: Identifies mailboxes with external email forwarding configured
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 SCRIPT HIGHLIGHTS:
