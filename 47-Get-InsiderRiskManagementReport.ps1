cba8502b702fc9a8aaae33782aee14aeac70756d
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 19:56:29 -0600

    Update Last Updated date

--- 47-Get-InsiderRiskManagementReport.ps1
+++ 47-Get-InsiderRiskManagementReport.ps1
@@ -3,7 +3,7 @@
 Script Name: 47-Get-InsiderRiskManagementReport.ps1
 Description: Microsoft 365 Advanced Reporting Script 47
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 REQUIREMENTS:
