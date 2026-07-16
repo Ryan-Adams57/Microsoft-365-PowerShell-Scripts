2bc6a78f2aa4540af5c42cb8e5e3c92884af543d
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 19:57:15 -0600

    Update Last Updated date

--- 48-Get-InformationBarriersReport.ps1
+++ 48-Get-InformationBarriersReport.ps1
@@ -3,7 +3,7 @@
 Script Name: 48-Get-InformationBarriersReport.ps1
 Description: Microsoft 365 Advanced Reporting Script 48
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 REQUIREMENTS:
