acf5ca6e9cdbf7cde231749134203d4c10c477f3
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 19:51:36 -0600

    Update Last Updated date

--- 41-Get-AzureADIdentityProtectionReport.ps1
+++ 41-Get-AzureADIdentityProtectionReport.ps1
@@ -3,7 +3,7 @@
 Script Name: 41-Get-AzureADIdentityProtectionReport.ps1
 Description: Azure AD Identity Protection policies and risk detections report
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 REQUIREMENTS:
