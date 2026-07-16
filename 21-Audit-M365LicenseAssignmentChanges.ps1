562e477cea9e6a455db72cb6af033f5609fe7a76
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-07-15 19:35:50 -0600

    Update Last Updated date

--- 21-Audit-M365LicenseAssignmentChanges.ps1
+++ 21-Audit-M365LicenseAssignmentChanges.ps1
@@ -3,7 +3,7 @@
 Script Name: 21-Audit-M365LicenseAssignmentChanges.ps1
 Description: License Assignment Change Tracking and Audit Report
 Version: 2.0 - Production Ready
-Last Updated: 2026-01-28
+Last Updated: 2026-07-15
 ====================================================================================
 
 REQUIREMENTS:
